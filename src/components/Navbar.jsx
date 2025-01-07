969a4614f85cdf70f62349e08214aefe593a0f3f
IEMDomain04.dc <emmanman0011@gmail.com>
2025-01-07 22:45:51 +0800

    Fixed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 
+const scrollToBento = () => {
+    document.getElementById('bento-section').scrollIntoView({ behavior: 'smooth', block: 'center' });
+};
+
+const scrollToProjects = () => {
+    document.getElementById('project-section').scrollIntoView({ behavior: 'smooth', block: 'center' });
+};
 const navs = [
-    { name: "Home", src: "", href: "" },
-    { name: "About", src: "", href: "" },
-    { name: "Projects", src: "", href: "" },
+    { name: "Home", src: "", onClick: "" },
+    { name: "About", src: "", onClick: scrollToBento },
+    { name: "Projects", src: "", onClick: scrollToProjects },
 ]
 
 const Navbar = () => {
     return (
         <main className='top-3 z-50 sticky justify-items-center'>
             <section className='w-fit h-fit mt-5 space-x-5 py-3 px-5 rounded-lg border bg-black border-slate-700 '>
-            {navs.map((nav, index) => (
-                <a className='text-xs rounded duration-300 hover:bg-slate-800 hover:p-2' href={nav.href}>{nav.name}</a>
-            ))}
+                {navs.map((nav, index) => (
+                    <a href={nav.href}><button onClick={nav.onClick} className='text-xs rounded duration-300 hover:bg-slate-800 hover:p-2' type="button">{nav.name}</button></a>
+                ))}
             </section>
         </main>
     )
---
+++ src/page/ProjectPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectPage = () => {
+  return (
+    <main>
+      
+    </main>
+  )
+}
+
+export default ProjectPage
