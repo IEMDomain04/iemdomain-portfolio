77b890c001dcdc47267a9c79b4250d42f2fa9442
IEMDomain04.dc <emmanman0011@gmail.com>
2025-04-28 12:43:55 +0800

    Added RAPID Case Study

--- src/Contents/Projects/ListProject.js
+++ src/Contents/Projects/ListProject.js
@@ -55,7 +55,7 @@ const projects = [
         title: "RAPID",
         year: "2024",
         description: "UX Case Study: A website for our business proposal in the subject The Entrepreneurial Mind.",
-        href: "https://docs.google.com/presentation/d/1H0eUeTsffrGpgM0-4ekE0vUyqOivGZS9neg8yWOg6oc/edit#slide=id.ge4c37861fa_0_354",
+        href: "https://ux-case-study-k6es3rc.gamma.site/",
         alt: "RAPID cover",
         role:"Developer - UI/UX Designer"
     },
