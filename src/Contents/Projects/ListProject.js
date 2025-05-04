7a8b1ba474bec0d783c4eae10f616bead5d24a98
IEMDomain04.dc <emmanman0011@gmail.com>
2025-05-04 21:25:18 +0800

    Changed the link of the ux case study rapid

--- src/Contents/Projects/ListProject.js
+++ src/Contents/Projects/ListProject.js
@@ -55,7 +55,7 @@ const projects = [
         title: "RAPID",
         year: "2024",
         description: "UX Case Study: A website for our business proposal in the subject The Entrepreneurial Mind.",
-        href: "https://ux-case-study-k6es3rc.gamma.site/",
+        href: "https://docs.google.com/presentation/d/1H0eUeTsffrGpgM0-4ekE0vUyqOivGZS9neg8yWOg6oc/edit?usp=sharing",
         alt: "RAPID cover",
         role:"Developer - UI/UX Designer"
     },
